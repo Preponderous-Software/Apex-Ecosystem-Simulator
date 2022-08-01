e615de2b9e9d97af3a3f6d88d1f1f2e5d0156093
dmccoystephenson <dmccoystephenson@gmail.com>
2022-07-31 19:27:09 -0600

    Added the Cow entity.

--- config.py
+++ config.py
@@ -10,28 +10,30 @@ class Config:
         # local
         grassFactor = random.randrange(1, 5)
         livingEntityFactor = 1
-        minGridSize = 10
-        maxGridSize = 20
+        minGridSize = 20
+        maxGridSize = 40
         minGrassGrowTime = 100
         maxGrassGrowTime = 500
 
         # static
-        self.displayWidth = 600
-        self.displayHeight = 600
+        self.displayWidth = 1080
+        self.displayHeight = 720
         self.tickSpeed = 1
         self.maxTickSpeed = 10
         self.black = (0,0,0)
         self.white = (255,255,255)
         self.brown = (170, 120, 0)
-        self.textSize = 20
         self.endSimulationUponAllLivingEntitiesDying = True
         self.autoRestart = True
         self.chanceToExcrete = 0.10
         self.chanceToReproduce = 0.02
+
         # calculated/random
         self.gridSize = random.randrange(minGridSize, maxGridSize)
+        self.textSize = ceil(self.displayHeight/37)
         self.numChickensToStart = ceil(self.gridSize*livingEntityFactor/1)
         self.numPigsToStart = ceil(self.gridSize*livingEntityFactor/2)
-        self.numWolvesToStart = ceil(self.gridSize*livingEntityFactor/4)
+        self.numCowsToStart = ceil(self.gridSize*livingEntityFactor/4)
+        self.numWolvesToStart = ceil(self.gridSize*livingEntityFactor/8)
         self.grassGrowTime = random.randrange(minGrassGrowTime, maxGrassGrowTime)
         self.numGrassEntities = self.gridSize*self.gridSize*grassFactor
