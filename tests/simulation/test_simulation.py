954efcfafe728781ca77af8d987ebc72480f8a82
dmccoystephenson <dmccoystephenson@gmail.com>
2024-09-18 19:55:56 -0600

    Added unit tests for addEntity(), removeEntityFromLocation() & printDeathInfo() to `test_simulation.py`

--- tests/simulation/test_simulation.py
+++ tests/simulation/test_simulation.py
@@ -1,7 +1,15 @@
 from unittest.mock import MagicMock, patch
 
+from entity.berries import Berries
 from entity.berryBush import BerryBush
+from entity.chicken import Chicken
+from entity.excrement import Excrement
+from lib.pyenvlib.location import Location
+import service
 from src.simulation import simulation
+
+# mock pygame
+service.soundService.pygame = MagicMock()
 
 def getTestSimulation():
     name = "my simulation"
@@ -10,8 +18,7 @@ def getTestSimulation():
     gameDisplay.get_size.return_value = (1080, 720)
     return simulation.Simulation(name, config, gameDisplay)
 
-@patch('service.soundService.pygame')
-def test_initialization(mock_pygame):
+def test_initialization():
     # prepare
     name = "my simulation"
     config = MagicMock()
@@ -26,9 +33,128 @@ def test_initialization(mock_pygame):
     assert testSim.config == config
     assert testSim.gameDisplay == gameDisplay
     assert testSim.environment is not None
+
+def test_initializeLocationWidthAndHeight():
+    # prepare
+    testSim = getTestSimulation()
+    testSim.environment = MagicMock()
+    testSim.environment.getGrid().getRows.return_value = 10
+    testSim.environment.getGrid().getColumns.return_value = 10
+     
+    # execute
+    testSim.initializeLocationWidthAndHeight()
     
-@patch('service.soundService.pygame')
-def test_growBerries(mock_pygame):
+    # assert
+    assert testSim.locationWidth == 108
+    assert testSim.locationHeight == 72
+
+def test_addEntity_Grass():
+    # prepare
+    testSim = getTestSimulation()
+    grass = MagicMock()
+    grass.getID.return_value = 1
+    
+    # execute
+    testSim.addEntity(grass)
+    
+    # assert
+    assert testSim.entities[1] == grass
+    assert 1 not in testSim.livingEntityIds
+    assert 1 not in testSim.excrementIds
+    assert 1 not in testSim.berryBushIds
+
+def test_addEntity_Chicken():
+    # prepare
+    testSim = getTestSimulation()
+    chicken = Chicken("test chicken")
+    
+    # execute
+    testSim.addEntity(chicken)
+    
+    # assert
+    assert testSim.entities[chicken.getID()] == chicken
+    assert chicken.getID() in testSim.livingEntityIds
+    assert chicken.getID() not in testSim.excrementIds
+    assert chicken.getID() not in testSim.berryBushIds
+
+def test_addEntity_Excrement():
+    # prepare
+    testSim = getTestSimulation()
+    tick = 1
+    excrement = Excrement(tick)
+    
+    # execute
+    testSim.addEntity(excrement)
+    
+    # assert
+    assert testSim.entities[excrement.getID()] == excrement
+    assert excrement.getID() not in testSim.livingEntityIds
+    assert excrement.getID() in testSim.excrementIds
+    assert excrement.getID() not in testSim.berryBushIds
+
+def test_addEntity_BerryBush():
+    # prepare
+    testSim = getTestSimulation()
+    berryBush = BerryBush()
+    
+    # execute
+    testSim.addEntity(berryBush)
+    
+    # assert
+    assert testSim.entities[berryBush.getID()] == berryBush
+    assert berryBush.getID() not in testSim.livingEntityIds
+    assert berryBush.getID() not in testSim.excrementIds
+    assert berryBush.getID() in testSim.berryBushIds
+
+def test_removeEntityFromLocation():
+    # prepare
+    testSim = getTestSimulation()
+    entity = MagicMock()
+    testSim.environment.getGrid().getLocation = MagicMock()
+    testSim.environment.getGrid().getLocation.return_value = MagicMock()
+    
+    # execute
+    testSim.removeEntityFromLocation(entity)
+    
+    # assert
+    testSim.environment.getGrid().getLocation.assert_called_once()
+    testSim.environment.getGrid().getLocation().removeEntity.assert_called_once()
+
+@patch("src.simulation.simulation.print")
+def test_printDeathInfo_notOldest(print):
+    # prepare
+    testSim = getTestSimulation()
+    entity = MagicMock()
+    entity.getName.return_value = "test entity"
+    oldestLivingEntity = MagicMock()
+    testSim.livingEntityIds = [1]
+    testSim.entities[1] = oldestLivingEntity
+    
+    # execute
+    testSim.printDeathInfo(entity, oldestLivingEntity)
+    
+    # assert
+    print.assert_called_once_with("test entity has died.")
+
+@patch("src.simulation.simulation.print")
+def test_printDeathInfo_isOldest(print):
+    # prepare
+    testSim = getTestSimulation()
+    entity = MagicMock()
+    entity.getName.return_value = "test entity"
+    entity.getID.return_value = 1
+    oldestLivingEntity = MagicMock()
+    oldestLivingEntity.getID.return_value = 1
+    testSim.livingEntityIds = [1]
+    testSim.entities[1] = oldestLivingEntity
+    
+    # execute
+    testSim.printDeathInfo(entity, oldestLivingEntity)
+    
+    # assert
+    print.assert_called_once_with("test entity has died. They were the oldest living entity.")
+
+def test_growBerries():
     # prepare
     testSim = getTestSimulation()
     berryBush = MagicMock()
@@ -43,8 +169,7 @@ def test_growBerries(mock_pygame):
     berryBush.incrementTick.assert_called_once()
     testSim.performBerryBushCheck.assert_called_once()
 
-@patch('service.soundService.pygame')
-def test_growBerries_noBerryBush(mock_pygame):
+def test_growBerries_noBerryBush():
     # prepare
     testSim = getTestSimulation()
     testSim.berryBushIds = []
@@ -56,8 +181,7 @@ def test_growBerries_noBerryBush(mock_pygame):
     # assert
     assert testSim.performBerryBushCheck.call_count == 0
 
-@patch('service.soundService.pygame')
-def test_performBerryBushCheck_notTime(mock_pygame):
+def test_performBerryBushCheck_notTime():
     # prepare
     testSim = getTestSimulation()
     berryBush = MagicMock()
@@ -72,8 +196,7 @@ def test_performBerryBushCheck_notTime(mock_pygame):
     # assert
     testSim.environment.getGrid().getLocation.assert_not_called()
 
-@patch('service.soundService.pygame')
-def test_performBerryBushCheck_notEnoughEnergy(mock_pygame):
+def test_performBerryBushCheck_notEnoughEnergy():
     # prepare
     testSim = getTestSimulation()
     berryBush = MagicMock()
@@ -89,8 +212,7 @@ def test_performBerryBushCheck_notEnoughEnergy(mock_pygame):
     # assert
     testSim.environment.getGrid().getLocation.assert_not_called()
 
-@patch('service.soundService.pygame')
-def test_performBerryBushCheck_tooManyBerries(mock_pygame):
+def test_performBerryBushCheck_tooManyBerries():
     # prepare
     testSim = getTestSimulation()
     berryBush = MagicMock()
@@ -108,8 +230,7 @@ def test_performBerryBushCheck_tooManyBerries(mock_pygame):
     # assert
     testSim.addEntity.assert_not_called()
 
-@patch('service.soundService.pygame')
-def test_performBerryBushCheck_Success(mock_pygame):
+def test_performBerryBushCheck_Success():
     # prepare
     testSim = getTestSimulation()
     berryBush = MagicMock()
@@ -126,16 +247,3 @@ def test_performBerryBushCheck_Success(mock_pygame):
     
     # assert
     testSim.addEntity.assert_called_once()
-
-@patch('service.soundService.pygame')
-def countBerriesInLocation(mock_pygame):
-    # prepare
-    testSim = getTestSimulation()
-    location = MagicMock()
-    location.getEntities.return_value = [BerryBush(), BerryBush()]
-    
-    # execute
-    result = testSim.countBerriesInLocation(location)
-    
-    # assert
-    assert result == 2
