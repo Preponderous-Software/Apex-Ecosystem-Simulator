288455c1454854430eb5d14f846d11571e9960d8
dmccoystephenson <dmccoystephenson@gmail.com>
2024-09-18 23:06:56 -0600

    Reorganized simulation unit tests & implemented more

--- tests/simulation/test_simulation.py
+++ tests/simulation/test_simulation.py
@@ -4,6 +4,7 @@ from entity.berries import Berries
 from entity.berryBush import BerryBush
 from entity.chicken import Chicken
 from entity.excrement import Excrement
+from entity.grass import Grass
 from lib.pyenvlib.location import Location
 import service
 from src.simulation import simulation
@@ -11,6 +12,7 @@ from src.simulation import simulation
 # mock pygame
 service.soundService.pygame = MagicMock()
 
+# helper methods -------------------------------------------------------------
 def getTestSimulation():
     name = "my simulation"
     config = MagicMock()
@@ -18,6 +20,7 @@ def getTestSimulation():
     gameDisplay.get_size.return_value = (1080, 720)
     return simulation.Simulation(name, config, gameDisplay)
 
+# constructor tests ----------------------------------------------------------
 def test_initialization():
     # prepare
     name = "my simulation"
@@ -34,6 +37,7 @@ def test_initialization():
     assert testSim.gameDisplay == gameDisplay
     assert testSim.environment is not None
 
+# public method tests --------------------------------------------------------
 def test_initializeLocationWidthAndHeight():
     # prepare
     testSim = getTestSimulation()
@@ -106,6 +110,31 @@ def test_addEntity_BerryBush():
     assert berryBush.getID() not in testSim.excrementIds
     assert berryBush.getID() in testSim.berryBushIds
 
+# def test_generateMap():
+#     assert False
+    
+# def test_placeEntities():
+#     assert False
+
+# def test_getNumberOfEntitiesOfType():
+#     assert False
+
+# def test_getNumberOfLivingEntitiesOfType():
+#     assert False
+    
+# def test_getNumLivingEntities():
+#     assert False
+
+# def test_getNumExcrement():
+#     assert False
+
+# def test_cleanup():
+#     assert False
+
+# def test_update():
+#     assert False
+
+# private method tests -------------------------------------------------------
 def test_removeEntityFromLocation():
     # prepare
     testSim = getTestSimulation()
@@ -153,6 +182,108 @@ def test_printDeathInfo_isOldest(print):
     
     # assert
     print.assert_called_once_with("test entity has died. They were the oldest living entity.")
+
+def test_removeEntity():
+    # prepare
+    testSim = getTestSimulation()
+    entity = MagicMock()
+    entity.getID.return_value = 1
+    testSim.entities[1] = entity
+    testSim.livingEntityIds = []
+    testSim.excrementIds = []
+    testSim.berryBushIds = []
+    testSim.removeEntityFromLocation = MagicMock()
+    
+    # execute
+    testSim.removeEntity(entity)
+    
+    # assert
+    assert 1 not in testSim.entities
+    testSim.removeEntityFromLocation.assert_called_once()
+
+def test_removeEntity_Excrement():
+    # prepare
+    testSim = getTestSimulation()
+    entity = Excrement(1)
+    testSim.entities[entity.getID()] = entity
+    testSim.livingEntityIds = []
+    testSim.excrementIds = [entity.getID()]
+    testSim.berryBushIds = []
+    testSim.removeEntityFromLocation = MagicMock()
+    
+    # execute
+    testSim.removeEntity(entity)
+    
+    # assert
+    assert entity.getID() not in testSim.entities
+    testSim.removeEntityFromLocation.assert_called_once()
+    assert entity.getID() not in testSim.excrementIds
+
+def test_removeEntity_BerryBush():
+    # prepare
+    testSim = getTestSimulation()
+    entity = BerryBush()
+    testSim.entities[entity.getID()] = entity
+    testSim.livingEntityIds = []
+    testSim.excrementIds = []
+    testSim.berryBushIds = [entity.getID()]
+    testSim.removeEntityFromLocation = MagicMock()
+    
+    # execute
+    testSim.removeEntity(entity)
+    
+    # assert
+    assert entity.getID() not in testSim.entities
+    testSim.removeEntityFromLocation.assert_called_once()
+    assert entity.getID() not in testSim.berryBushIds
+
+def test_removeEntity_Chicken_NotMuted():
+    # prepare
+    testSim = getTestSimulation()
+    entity = Chicken("test chicken")
+    testSim.entities[entity.getID()] = entity
+    testSim.livingEntityIds = [entity.getID()]
+    testSim.excrementIds = []
+    testSim.berryBushIds = []
+    testSim.removeEntityFromLocation = MagicMock()
+    testSim.soundService = MagicMock()
+    testSim.config.muted = False
+    
+    # execute
+    testSim.removeEntity(entity)
+    
+    # assert
+    assert entity.getID() not in testSim.entities
+    testSim.removeEntityFromLocation.assert_called_once()
+    assert entity.getID() not in testSim.livingEntityIds
+    testSim.soundService.playDeathSoundEffect.assert_called_once()
+
+def test_removeEntity_Chicken_Muted():
+    # prepare
+    testSim = getTestSimulation()
+    entity = Chicken("test chicken")
+    testSim.entities[entity.getID()] = entity
+    testSim.livingEntityIds = [entity.getID()]
+    testSim.excrementIds = []
+    testSim.berryBushIds = []
+    testSim.removeEntityFromLocation = MagicMock()
+    testSim.soundService = MagicMock()
+    testSim.config.muted = True
+    
+    # execute
+    testSim.removeEntity(entity)
+    
+    # assert
+    assert entity.getID() not in testSim.entities
+    testSim.removeEntityFromLocation.assert_called_once()
+    assert entity.getID() not in testSim.livingEntityIds
+    testSim.soundService.playDeathSoundEffect.assert_not_called()
+
+# def test_performExcrementCheck():
+#     assert False
+
+# def test_growGrass():
+#     assert False
 
 def test_growBerries():
     # prepare
@@ -247,3 +378,55 @@ def test_performBerryBushCheck_Success():
     
     # assert
     testSim.addEntity.assert_called_once()
+
+def test_countBerriesInLocation():
+    # prepare
+    testSim = getTestSimulation()
+    location = Location(1, 1)
+    berries = Berries()
+    location.addEntity(berries)
+    testSim.environment.getEntity = MagicMock()
+    testSim.environment.getEntity.return_value = berries
+    
+    # execute
+    result = testSim.countBerriesInLocation(location)
+    
+    # assert
+    assert result == 1
+
+# def test_initiateEntityActions():
+#     assert False
+
+def test_decreaseEnergyForLivingEntities():
+    # prepare
+    testSim = getTestSimulation()
+    chicken = Chicken("test chicken")
+    chicken.decreaseEnergy = MagicMock()
+    testSim.entities[chicken.getID()] = chicken
+    testSim.livingEntityIds = [chicken.getID()]
+    chicken.removeEnergy = MagicMock()
+    
+    # execute
+    testSim.decreaseEnergyForLivingEntities()
+    
+    # assert
+    chicken.removeEnergy.assert_called_once_with(1)
+
+def test_decreaseEnergyForLivingEntities_OutOfEnergy():
+    # prepare
+    testSim = getTestSimulation()
+    chicken = Chicken("test chicken")
+    chicken.decreaseEnergy = MagicMock()
+    chicken.getEnergy = MagicMock()
+    chicken.getEnergy.return_value = 0
+    chicken.removeEnergy = MagicMock()
+    testSim.entities[chicken.getID()] = chicken
+    testSim.livingEntityIds = [chicken.getID()]
+    testSim.removeEntity = MagicMock()
+    
+    # execute
+    testSim.decreaseEnergyForLivingEntities()
+    
+    # assert
+    chicken.removeEnergy.assert_called_once_with(1)
+    testSim.removeEntity.assert_called_once_with(chicken)
